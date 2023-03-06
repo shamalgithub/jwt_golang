487f6df2518d4f56c5dcae6a064363ab72778680
shamalgithub <shamalgithub@gmail.com>
2023-03-06 02:59:46 -0800

    rest completed

--- Go_tutorial_1/main.go
+++ Go_tutorial_1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
@@ -35,11 +36,56 @@ func addTodos(context *gin.Context){
 
 }
 
+
+func todosByQuery( context *gin.Context){
+	id , okay := context.GetQuery("id")
+	if !okay {
+		context.IndentedJSON(http.StatusBadRequest , gin.H{"message":"Missing id query parameter"})
+
+	}else{
+		todos , err := getTodosbyId(id)
+		if err != nil {
+			context.IndentedJSON(http.StatusNotFound , gin.H{"message":"Todo not found"})
+			return 
+		}else{
+			context.IndentedJSON(http.StatusOK , todos)
+		}
+	}
+
+}
+
+
+
+
+func todoById(context *gin.Context){
+	id := context.Param("id")
+	todoItem , err := getTodosbyId(id)
+
+	if err !=nil{
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message":"todo not found"} )
+		return 
+	}else{
+		context.IndentedJSON(http.StatusOK , todoItem)
+	}
+}
+
+func getTodosbyId(id string)(*todo , error){
+	for index , td := range todos {
+		if td.ID == id {
+			return &todos[index] , nil
+		}
+	}
+	return nil , errors.New("todo item not found")
+}
+
+
 func main(){
 	
 	router := gin.Default()
 	router.GET("/todo" , getTodos)
 	router.POST("/post-todos" , addTodos)
+	router.GET("/todo/:id" , todoById)
+	router.GET("/todos" , todosByQuery)
 	router.Run("localhost:8080")
 	
 	
